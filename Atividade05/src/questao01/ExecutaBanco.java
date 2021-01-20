88f85742c62d5b5858bcc827ceb0fadef3bfcf2b
EzequielCosta <62070653+EzequielCosta@users.noreply.github.com>
2021-01-20 17:38:40 -0300

    Add files via upload

---
+++ Atividade05/src/questao01/ExecutaBanco.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+package questao01;
+
+// Irá dar erro , pois o package deve ser definido antes do import
+public class ExecutaBanco {
+    public static void main(String[] args) {
+        
+    }
+}
