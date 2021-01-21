f5869efbaedbbdc224abd8fad982b661fbf11e94
EzequielCosta <62070653+EzequielCosta@users.noreply.github.com>
2021-01-21 01:14:26 -0300

    Add files via upload

---
+++ Atividade6P1/src/QuestoesSubjetivas.java
@@ -0,0 +1,16 @@
+
+public class QuestoesSubjetivas {
+ 
+        /*
+        
+    11 -> V V V V V;
+    12 -> "metodo 1, classe B";
+    13 -> Se a classe especializada é instaciada com o tipo da classe geral, ela somente poderá acessar os método 
+    e atributos da classe mãe;
+    14 -> iria chamar o metodo1 da classe mãe;
+    16 -> para que haja sobrecarga deve os metodos devem possuir mesmo nome, retornos diferentes
+     e tipos de argumentos diferente, nesse caso, o retorno e a falta de parametros, torna os métodos iguais
+    17 -> O método já foi definido mais acima pois possui o mesmo nome e parametros de tipos iguas,
+     apenas o retorno é diferente;
+    */
+}
